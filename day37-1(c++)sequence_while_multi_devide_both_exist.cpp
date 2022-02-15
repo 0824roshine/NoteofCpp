85ff0f908fd852c72b96730936fccd9540993246
0824roshine <96732376+0824roshine@users.noreply.github.com>
2022-02-15 23:54:37 +0800

    Add files via upload

---
+++ day37-1(c++)sequence_while_multi_devide_both_exist.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float a=22.1;
+	float b = 1.3;
+	int c = 12;
+	int d = 5;
+	int e = 3;
+	cout <<  "a+b= " << a+b << endl;
+	cout << "c*d/e = " << c * d / e << endl;
+	return 0;
+}
